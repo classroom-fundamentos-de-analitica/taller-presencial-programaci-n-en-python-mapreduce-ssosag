80ec8f232b3b6a4c89f4d13daf0f2316af09ba01
ssosag <ssosag@unal.edu.co>
2024-02-12 11:15:57 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -13,8 +13,22 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
+import glob
+import fileinput
+from itertools import groupby
+import os.path
+
+
 def load_input(input_directory):
-    pass
+    filenames = glob.glob(input_directory + "/*.*")
+
+    sequence = []
+
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            sequence.append((f.filename(), line))
+
+    return sequence
 
 
 #
@@ -30,7 +44,12 @@ def load_input(input_directory):
 #   ]
 #
 def mapper(sequence):
-    pass
+    new_sequence = [
+        (word.lower().replace(".", "").replace(",", ""), 1)
+        for _, line in sequence
+        for word in line.split()
+    ]
+    return new_sequence
 
 
 #
@@ -45,7 +64,8 @@ def mapper(sequence):
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sequence = sorted(sequence, key=lambda x: x[0])
+    return sequence
 
 
 #
@@ -55,7 +75,12 @@ def shuffle_and_sort(sequence):
 # texto.
 #
 def reducer(sequence):
-    pass
+    new_sequence = []
+    for k, g in groupby(sequence, lambda x: x[0]):
+        key = k
+        values = sum(x[1] for x in g)
+        new_sequence.append((key, values))
+    return new_sequence
 
 
 #
@@ -63,7 +88,9 @@ def reducer(sequence):
 # y lo crea. Si el directorio existe, la función falla.
 #
 def create_ouptput_directory(output_directory):
-    pass
+    if os.path.isdir(output_directory):
+        raise Exception("El directorio ya existe")
+    os.mkdir(output_directory)
 
 
 #
@@ -75,7 +102,11 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    filename = os.path.join(output_directory, "part-00000")
+
+    with open(filename, "w") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -83,14 +114,21 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open(os.path.join(output_directory, "_SUCCESS"), "w") as f:
+        f.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 
 if __name__ == "__main__":
